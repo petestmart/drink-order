30a3d2ecd9a811832668aa32568c947ec347a0bd
Pete St. Martin <peter.r.stmartin@gmail.com>
2019-09-21 11:14:49 -0500

    Cards on page. Centered. Spaced evenly

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,17 +1,34 @@
 import React from 'react';
-// import logo from './logo.svg';
 import './App.css';
 import DrinkCards from '../DrinkCards/DrinkCards';
+import Grid from "@material-ui/core/Grid";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <h1>Drink Ordering System</h1>
+        <h1>Drink Ordering System</h1>
       </header>
       <body>
         <h2>Select a Beverage</h2>
-        <DrinkCards/>
+        <div className="Select-beverage">
+          <Grid
+            container spacing={24}
+            alignItems="center"
+            justify="center"
+            spacing={2}
+          >
+            <Grid item md={3}>
+              <DrinkCards />
+            </Grid>
+            <Grid item md={3}>
+              <DrinkCards />
+            </Grid>
+            <Grid item md={3}>
+              <DrinkCards />
+            </Grid>
+          </Grid>
+        </div>
       </body>
     </div>
   );
